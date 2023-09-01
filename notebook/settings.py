d6bceb9d6c21a400b1d219342fc14c2a36c29a0c
resistansepower <resistansepower@gmail.com>
2023-09-01 13:57:25 +0300

    secret key refactor

--- notebook/settings.py
+++ notebook/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9^fvjkr*xv*v*_h%o#%exrn9=s7k$s1ukzhe__#b37wpus^_@m'
+SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
