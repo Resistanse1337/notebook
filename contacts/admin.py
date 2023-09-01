c51d28c5502542fb983385ea82a75e2d37ba696b
resistansepower <resistansepower@gmail.com>
2023-09-01 13:46:55 +0300

    refactor

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from contacts.models import Contact
 
 
-
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ("id", "first_name", "last_name", "phone_number", "email", "created_at",)
